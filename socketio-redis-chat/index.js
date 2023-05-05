b3a8936c445d9cfcaa8ec438e5ac19e8b711ffbb
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-05-05 23:15:58 +0530

    add messages to redis and retrives to send these messages to new joinees

--- socketio-redis-chat/index.js
+++ socketio-redis-chat/index.js
@@ -2,9 +2,15 @@ const path = require('path')
 const http = require('http')
 const express = require("express")
 const { Server } = require('socket.io')
-const { log } = require('console')
 const app = express()
+const redis = require('redis')
 const PORT = 5000 || process.env.PORT
+const redisClient = redis.createClient(6379, '127.0.0.1')
+try {
+    redisClient.connect()
+} catch (error) {
+    console.log(error);
+}
 
 app.set("view engine", "ejs")
 app.get("/chat", (req, res) => {
@@ -21,11 +27,31 @@ const httpServer = http.createServer(app)
 const ioServer = new Server(httpServer)
 
 ioServer.on('connection', socket => {
-    socket.on('message', ({ message, from }) => {
-        console.log(message, from);
-        ioServer.emit('message',{message, from})
+    sendMessage(socket)
+    socket.on('message', ({ from, message }) => {
+        console.log(from, message);
+        addMessageToRedis(from, message)
+        ioServer.emit('message', { from, message })
     })
 })
 
+async function addMessageToRedis(from, message) {
+    try {
+        await redisClient.hSet('messages', from, message)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function sendMessage(socket) {
+    const data = await redisClient.hGetAll('messages')
+    for (const from in data) {
+        if (Object.hasOwnProperty.call(data, from)) {
+            const message = data[from];
+            socket.emit('message', { from, message })
+        }
+    }
+    console.log(data);
+}
 
 httpServer.listen(PORT, () => console.log(`Server started at port ${PORT}`))
